208dfdc8336ba6016e166ef78143a1c627d02750
dilmithyoshitha <107747067+dilmithyoshitha@users.noreply.github.com>
2022-06-18 13:47:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94775013191']
+global.premium = ['94775013191']
+global.ownernomer = '94775013191'
+global.ownername = 'Jupiter MD🍃'
+global.botname = 'Jupiter MD🍃'
+global.footer = 'Jupier MD'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'Sri lanka, southern province, galla'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.author = 'jupiterbotofficial'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
